ed6d7233a90f7b6bdb01559e731e017abd6c1eaf
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-05 19:34:14 +0530

    Create amd-plugin.sh
    bash script to download and install the plugins

---
+++ Dockercontainer/amd-plugin.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/chamohan/scancode-plugins.git
+cd scancode-plugins; pip install ./scancode-licence-modifications
+pip install ./scancode-no-licenses
+pip install ./scancode-only-licenses-titles
+pip install ./scancode-keywords-scan
