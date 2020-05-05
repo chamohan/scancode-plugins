9d33ce968546a4bd2ba08ed41e14fe50d12f00cd
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-05 14:17:24 +0530

    Create amd-plugin.sh

---
+++ amd-plugin.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export GIT=`which git`
+export PIP=`which pip`
+
+${GIT} clone https://github.com/chamohan/scancode-plugins.git
+${PIP} install -e scancode-plugins/scancode-keywords-scan/
+${PIP} install -e scancode-plugins/scancode-licence-modifications/
+${PIP} install -e scancode-plugins/scancode-no-licenses/
+{$PIP} install -e scancode-plugins/scancode-only-licenses-titles}/
