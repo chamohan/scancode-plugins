e55f93c73e7e13aeef02044114ee0b631dbe9c9e
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-05 15:42:21 +0530

    Update amd-plugin.sh

--- amd-plugin.sh
+++ amd-plugin.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/chamohan/scancode-plugins.git
 cd scancode-plugins; pip install ./scancode-licence-modifications
-cd scancode-plugins;pip install ./scancode-no-licenses
-cd scancode-plugins;pip install ./scancode-only-licenses-titles
-cd scancode-plugins;pip install ./scancode-keywords-scan
+pip install ./scancode-no-licenses
+pip install ./scancode-only-licenses-titles
+pip install ./scancode-keywords-scan
