9678e605ca8fbf8be4f2f272a525e1b75be100e8
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-07 15:12:35 +0530

    Update scancodestatus.py

--- scancodestatus.py
+++ scancodestatus.py
@@ -19,6 +19,58 @@ with open(filename) as f:
     modifications_counter = 0
     for i in data['files']:
         if i['keywordsline']:
+            print("File Name %s" %(i['path']))
+            print("Number of keywords %d" %(i['keywordsline']))
+            for m in i['matchedlines']:
+                print(m)
+            counter = counter + 1
+
+        if i['licenses'] is None:
+            no_license = no_license + 1
+        else:
+            for j in i['licenses']:
+                try:
+                    if j['score'] != 100.0:
+                        #print(j['score'])
+                        #print(i['path'])
+                        #print(i['licenses'])
+                        #print(i['licence_modifications'])
+                        modifications_counter = modifications_counter + 1
+                except Exception:
+                    pass
+
+
+if modifications_counter > 0:
+    print("The total number of files containing no linceses are %d" %(no_license))
+    print("Number of linceses Modifications %d" %(modifications_counter))
+    print("The number of times keywords present in files %s" %(counter))
+    print("Failed")
+    sys.exit(1)
+else:
+    print("Tests Passed")
+
+    
+#!/usr/bin/env python
+# In case of any issue please write chamohan@amd.com
+
+import json
+import sys
+import os
+
+path = '/artifacts'
+os.chdir(path)
+files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+no_license = 0
+oldest = files[0]
+#latest  json file name
+filename = files[-2]
+counter = 0
+
+with open(filename) as f:
+    data = json.load(f)
+    modifications_counter = 0
+    for i in data['files']:
+        if i['keywordsline']:
             print("keywords present")
             print(i['keywordsline'])
             print(i['matchedlines'])
