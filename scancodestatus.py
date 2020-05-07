b3597404acf30e7fddffb371702e7fae93d9b786
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-07 14:50:21 +0530

    Update scancodestatus.py

--- scancodestatus.py
+++ scancodestatus.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # In case of any issue please write chamohan@amd.com
+
 import json
 import sys
 import os
@@ -7,31 +8,44 @@ import os
 path = '/artifacts'
 os.chdir(path)
 files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
-
+no_license = 0
 oldest = files[0]
-#latest  json output file name fron /aritifacts path
+#latest  json file name
 filename = files[-2]
+counter = 0
 
 with open(filename) as f:
-  data = json.load(f)
-modifications_counter = 0
-for i in data['files']:
-   if i['licenses'] is None:
-      print("NoLicence")
-   else:
-     for j in i['licenses']:
-        try:
-           if j['score'] != 100.0:
-              print(j['score'])
-              print(i['path'])
-              print(i['licenses'])
-              print(i['licence_modifications'])
-              modifications_counter = modifications_counter + 1
-        except Exception:
-           pass
+    data = json.load(f)
+    modifications_counter = 0
+    for i in data['files']:
+        if i['keywordsline']:
+            print("keywords present")
+            print(i['keywordsline'])
+            print(i['matchedlines'])
+            counter = counter + 1
+        else:
+            print("not present")
+
+        if i['licenses'] is None:
+            no_license = no_license + 1
+        else:
+            for j in i['licenses']:
+                try:
+                    if j['score'] != 100.0:
+                        #print("file found")
+                        #print(j['score'])
+                        #print(i['path'])
+                        #print(i['licenses'])
+                        #print(i['licence_modifications'])
+                        modifications_counter = modifications_counter + 1
+                except Exception:
+                    pass
 
 
 if modifications_counter > 0:
+    print("The total number of files containing no linceses are %d" %(no_license))
+    print("Number of linceses Modifications %d" %(modifications_counter))
+    print("The number of times keywords present in files %s" %(counter))
     print("Failed")
     sys.exit(1)
 else:
