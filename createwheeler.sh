106bc5d45e67774cdc032bd87f59a34dedd13766
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-15 21:47:30 +0530

    Update createwheeler.sh

--- createwheeler.sh
+++ createwheeler.sh
@@ -4,7 +4,7 @@ mkdir -p ${PWD}/Containers/Docker/plugins
 pluginDir="${PWD}/Containers/Docker/plugins"
 
 
-for d in ${PWD}/Containers/Docker/amd-scancode/scancode-*/;do
+for d in ${PWD}/plugins/scancode-*/;do
       cd "${d}";/bin/bash -c "(python3 setup.py bdist_wheel)"
       sleep 2
       cp -R ${d}/dist/* ${pluginDir}
