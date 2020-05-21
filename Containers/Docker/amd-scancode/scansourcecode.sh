9336430fe74c2af92b18b95c8acba219966110ae
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-21 18:24:32 +0530

    Update scansourcecode.sh

--- Containers/Docker/amd-scancode/scansourcecode.sh
+++ Containers/Docker/amd-scancode/scansourcecode.sh
@@ -1,7 +1,11 @@
-scancode  -clpeui  --package --processes `expr $(nproc --all) - 1` --classify --keywordsscan --verbose --full-root --json-pp /artifacts/$(date "+%Y.%m.%d-%H.%M.%S")-licenses.json /src --license-policy /amd-scancode/amd_licence_policy.yml --summary --summary-with-details --license-text --license-text-diagnostics --is-license-text  --license-diag  --no-licenses  --licence-modifications --custom-output /artifacts/$(date "+%Y.%m.%d-%H.%M.%S")-license-modification-report.html --custom-template /amd-scancode/license-modification-template.html >>/logs/$(date "+%Y.%m.%d-%H.%M.%S")-logfile 2>&1
+export SCANCODE=`which scancode`
+export PYTHON=`which python`
+
+type -P python3 >/dev/null 2>&1 && echo Python 3 is installed
+
+
+${SCANCODE}  -clpeui  --package --processes `expr $(nproc --all) - 1` --classify --keywordsscan --verbose --full-root --json-pp /artifacts/$(date "+%Y.%m.%d-%H.%M.%S")-licenses.json /src --license-policy /amd-scancode/amd_licence_policy.yml --summary --summary-with-details --license-text --license-text-diagnostics --is-license-text  --license-diag  --no-licenses  --licence-modifications --custom-output /artifacts/$(date "+%Y.%m.%d-%H.%M.%S")-license-modification-report.html --custom-template /amd-scancode/license-modification-template.html >>/logs/$(date "+%Y.%m.%d-%H.%M.%S")-logfile 2>&1
 
 # running analysis
 
-exec python /amd-scancode/scancodestatus.py >/statistics/$(date "+%Y.%m.%d-%H.%M.%S")-logfile 2>&1
-echo $?
-
+${PYTHON} /amd-scancode/scancodestatus.py >/statistics/$(date "+%Y.%m.%d-%H.%M.%S")-logfile 2>&1
