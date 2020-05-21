612b930792655c21af8dcc45105cb0808009fa02
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-21 17:50:51 +0530

    Update createwheeler.sh

--- createwheeler.sh
+++ createwheeler.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 export PWD=`pwd`
-mkdir -p ${PWD}/Containers/Docker/plugins
-pluginDir="${PWD}/Containers/Docker/plugins"
+export MKDIR=`which mkdir`
+export PYTHON3=`which python3`
+export CP=`which cp`
+export pluginDir="${PWD}/Containers/Docker/plugins"
 
 
-for d in ${PWD}/plugins/scancode-*/;do
-      cd "${d}";/bin/bash -c "(python3 setup.py bdist_wheel)"
-      sleep 2
-      cp -R ${d}/dist/* ${pluginDir}
-done
+function buildPlugins {
+
+    ${MKDIR} -p ${PWD}/Containers/Docker/plugins
+
+    type -P python3 >/dev/null 2>&1 && echo Python 3 is installed
+
+    if [ $? -eq 1 ];then
+        echo "python3 not installed or change the environment to python3"
+        exit 1
+    fi
+    for d in ${PWD}/plugins/scancode-*/;do
+        cd "${d}";/bin/bash -c "(python3 setup.py bdist_wheel)"
+        sleep 2
+        ${CP} -R ${d}/dist/* ${pluginDir}
+    done
+}
+
+
+buildPlugins
