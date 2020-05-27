7ca2c89b06ceaa23eefa094909f808e13d2aa3f7
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-27 17:50:05 +0530

    Update scansourcecode.sh

--- Containers/Docker/amd-scancode/scansourcecode.sh
+++ Containers/Docker/amd-scancode/scansourcecode.sh
@@ -1,11 +1,34 @@
+#!/usr/bin/env bash
+#For any issue please write to chamohan@amd.com
+
 export SCANCODE=`which scancode`
 export PYTHON=`which python`
+export SOURCECODE="/src"
+export LOGS="/logs"
+export ARTIFACTS="/artifacts"
+export LICENSEPOLICY="/amd-scancode/amd_licence_policy.yml"
+export LICENSETEMPLATE="/amd-scancode/license-modification-template.html"
+export STATUSSCRIPT="/amd-scancode/scancodestatus.py"
+export STATISTICS="/statistics"
 
-type -P python3 >/dev/null 2>&1 && echo Python 3 is installed
+trap cleanup 3 6 15
+
+cleanup()
+{
+  echo "Caught Signal ... cleaning up." >>${LOGS}/$(date "+%Y.%m.%d-%H.%M.%S")-cleanup
+  echo "Done cleanup ... quitting. process id $$ " >>${LOGS}/$(date "+%Y.%m.%d-%H.%M.%S")-cleanup
+  exit 1
+}
 
 
-${SCANCODE}  -clpeui  --package --processes `expr $(nproc --all) - 1` --classify --keywordsscan --verbose --full-root --json-pp /artifacts/$(date "+%Y.%m.%d-%H.%M.%S")-licenses.json /src --license-policy /amd-scancode/amd_licence_policy.yml --summary --summary-with-details --license-text --license-text-diagnostics --is-license-text  --license-diag  --no-licenses  --licence-modifications --custom-output /artifacts/$(date "+%Y.%m.%d-%H.%M.%S")-license-modification-report.html --custom-template /amd-scancode/license-modification-template.html >>/logs/$(date "+%Y.%m.%d-%H.%M.%S")-logfile 2>&1
+function runscan {
 
-# running analysis
 
-${PYTHON} /amd-scancode/scancodestatus.py >/statistics/$(date "+%Y.%m.%d-%H.%M.%S")-logfile 2>&1
+    ${SCANCODE}  -clpeui  --package --processes `expr $(nproc --all) - 1` --classify --keywordsscan --verbose --full-root --json-pp ${ARTIFACTS}/$(date "+%Y.%m.%d-%H.%M.%S")-licenses.json ${SOURCECODE} --license-policy ${LICENSEPOLICY} --summary --summary-with-details --license-text --license-text-diagnostics --is-license-text  --license-diag  --no-licenses  --licence-modifications --custom-output ${ARTIFACTS}/$(date "+%Y.%m.%d-%H.%M.%S")-license-modification-report.html --custom-template ${LICENSETEMPLATE} >>${LOGS}/$(date "+%Y.%m.%d-%H.%M.%S")-logfile 2>&1
+
+    # running analysis
+
+    ${PYTHON} ${STATUSSCRIPT} >>${STATISTICS}/$(date "+%Y.%m.%d-%H.%M.%S")-logfile 2>&1
+}
+
+runscan
