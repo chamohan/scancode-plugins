8b54d7379a9e11f4bfd145010ef1957244af1504
root <root@BuildSlave5.local.lan>
2020-05-27 06:15:45 -0400

    Deleted the files

--- Containers/Docker/amd-scancode/amd-plugin.sh
+++
@@ -1,5 +0,0 @@
-#git clone https://github.com/chamohan/scancode-plugins.git
-cd scancode-plugins; pip install ./scancode-licence-modifications
-pip install ./scancode-no-licenses
-pip install ./scancode-only-licenses-titles
-pip install ./scancode-keywords-scan
--- Containers/Docker/amd-scancode/gitsubmit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export GIT=`which git`
-${GIT} add ${1}
-${GIT} status
-${GIT} commit ${1} 
-${GIT} push origin master
-
