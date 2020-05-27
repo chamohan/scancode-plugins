86a0fae22fc033154d258af795a10d784b9eda9d
chamohan <63771787+chamohan@users.noreply.github.com>
2020-05-27 15:48:55 +0530

    Delete scancodeenv.bash

--- Containers/Docker/amd-scancode/scancodeenv.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Script to setup the scancode run and dev environment
-# for any issue please contact chamohan@amd.com
-
-set -e
-set -x
-export SCL=`which scl`
-cd scancode;source  python3-virtualenv/bin/activate
-${SCL} enable devtoolset-7 /bin/bash
