97a148fb05c9b8394d643aff49d1555ed2478a3f
chamohan <63771787+chamohan@users.noreply.github.com>
2020-06-28 21:51:59 +0530

    refactored the code
    Refactored the code for addremovelicense CLI

--- plugins/scancode-addremovelicenses/src/addremovelicenses.py
+++ plugins/scancode-addremovelicenses/src/addremovelicenses.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 import sys
+import yaml
+from shutil import copyfile
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout)
@@ -10,37 +12,94 @@ logger.setLevel(logging.DEBUG)
 class AddRemoveLicenses:
      
 
-    def __init__(self, licensefile, licensemetadatafile,):
+    def __init__(self, licensefile, licensemetadatafile):
 
-        self.licensefile = licensefile 
+        self.licensefile = licensefile
         self.licensemetadatafile = licensemetadatafile
 
-    def scanLogResults(self):
-        licensePolicy = 0
-        approvedLicenses = 0
-        prohibitedLicenses = 0
-        totalIssues = 0
-         
+    def extract_file_extension(self):
+        licenseFilepath= ""
+        metadataFilepath = ""
+        licenseFileExtension = ""
+        metadataFileExtension = ""
+        fileExtensions = []
+        licenseFilepath , licenseFileExtension = os.path.splitext(self.licensefile)
+        metadataFilepath, metadataFileExtension = os.path.splitext(self.licensemetadatafile)
+        fileExtensions.append(metadataFileExtension)
+        fileExtensions.append(licenseFileExtension)
+        return fileExtensions
+
+    def is_licencefile_notempty(self):
+        # Check if file exist and it is not empty
+        return os.path.exists(self.licensefile) and os.stat(self.licensefile).st_size != 0
+
+    def is_metadatafile_yaml(self, file_name):
+        # check if the file exist and it is a valid yaml
+        return os.path.exists(self.licensemetadatafile) and yaml.safe_load(self.licensemetadatafile)
+
+    def copy_files_scancode(self):
+        # Copy the files to scancode license location
+        # adding exception handling
+        try:
+            copyfile(self.licensefile, licenseFileTargetLocation)
+        except IOError as e:
+            print("Unable to copy file. %s" % e)
+            exit(1)
+        except:
+            print("Unexpected error:", sys.exc_info())
+            return(1)
+        try:
+            copyfile(self.licensemetadatafile, MetadataFileTargetLocation)
+        except IOError as e:
+            print("Unable to copy file. %s" % e)
+            exit(1)
+        except:
+            print("Unexpected error:", sys.exc_info())
+            return(1)
+
+    def error_message(errortype):
+        try:
+            if errortype == 'licensefile':
+                print ("Check the license file, it is either not present or empty ")
+            if errortype == 'metadatafile':
+                print( "Check the file, it is either not present of not correct yaml")
+        except ValueError as err:
+            logger.debug("Check the file")
+
+    def AddScanCodeLicense(self):
+        checklicense = ""
+        checkmetadata = ""
 
         try:
-            os.chdir(self.licensefile)
-            files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
-            no_license = 0
-            # latest  json report file name created with time stamp
-            filename = files[-1]
+            is_correct_extension = []
+            checklicense = self.is_licencefile_notempty(self)
+            checkmetadata = self.is_metadatafile_yaml(self)
+            if checklicense and checkmetadata:
+                is_correct_extension = self.extract_file_extension(self)
+                if is_correct_extension[0] == '.LICENSE' and is_correct_extension[1] == '.yml':
+                    self.copy_files_scancode()
+            else:
+                logger.debug("Files were not present")
+
+        except IOError as err:
+            logger.debug("Could not copy files")
+            logger.debug("")
+        except KeyboardInterrupt as err:
+            logger.debug('Caught KeyboardInterrupt')
+        except IndexError as err:
+            logger.debug("Index error")
         except OSError as err:
             logger.debug("OS error: {0}".format(err))
             logger.debug("Not able to find/list/sort the files")
         except ValueError as err:
             logger.debug("No JSON object could be decoded")
         except IndexError as err:
-            logger.debug("No log files present") 
-            totalIssues = totalIssues + 1
-            sys.exit(totalIssues)
-         
+            logger.debug("No log files present")
+            sys.exit("Run command Again")
+
 
         try:
-            with open(filename) as f:
+            with open(self.licensemetadatafile) as f:
                 data = json.load(f)
                 modifications_counter = 0
                 for i in data['files']:
@@ -51,57 +110,11 @@ class AddRemoveLicenses:
                         for m in i['matchedlines']:
                             keywordsCounter = keywordsCounter + 1
 
-                    # Checking Licenses in file
-
-                    if not i['no_licenses']:
-                        files_with_license = files_with_license + 1
-                    else:
-                        files_without_license = files_without_license + 1
-                        logger.debug("File Name %s is under %s License" % (i['path'], i['no_licenses']))
-
-                    # Checking Not Approved Licences and Approved License
-
-                    if not i['license_policy']:
-                        licensePolicy = licensePolicy + 1
-                    else:
                         if i['license_policy']['label'] == 'Approved License':
-                            approvedLicenses = approvedLicenses + 1
-                            logger.debug("The file %s has %s" % (i['path'], i['license_policy']['label']))
                         elif i['license_policy']['label'] == 'Prohibited Licenses':
-                            prohibitedLicenses = prohibitedLicenses + 1
-                            logger.debug("The file %s has %s" % (i['path'], i['license_policy']['label']))
-
-                    # Checking license Modifications in files
-
-                    if i['licenses'] is None:
-                        no_license = no_license + 1
-                    else:
-                        for j in i['licenses']:
-                            try:
-                                if j['score'] != 100.0:
-                                    modifications_counter = modifications_counter + 1
-                            except Exception:
-                                continue
-
-            if modifications_counter > 0:
-                totalIssues = (modifications_counter + keywordsCounter + prohibitedLicenses + no_license)
-                logger.debug("-----Summary Report------")
-                logger.debug("The total number of files containing no linceses are %d" % (no_license))
-                logger.debug("Number of linceses Modifications %d" % (modifications_counter))
-                logger.debug("The number of times keywords were present in files %s" % (keywordsCounter))
-                logger.debug("The number of Prohibited Licenses are %s" % (prohibitedLicenses))
-                logger.debug("The number of Approved Linceses are %s" % (approvedLicenses))
-                logger.debug("The Total number of issues are %s" % (totalIssues))
-                logger.debug("Failed")
-                sys.exit(totalIssues)
-            else:
-                logger.debug("The number of Approved Linceses are %s" % (approvedLicenses))
-                logger.debug("Tests Passed")
-                sys.exit(approvedLicenses)
-
         except RuntimeError:
             logger.debug("RuntimeError: {0}".format(err))
-            sys.exit(totalIssues)
+            sys.exit()
 
    
 
