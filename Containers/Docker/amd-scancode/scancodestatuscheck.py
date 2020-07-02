6e71c4e09463837970eda94f4adfa6dccf3b679a
chamohan <chamohan@amd.com>
2020-07-02 09:33:19 +0000

    Refactored the code

--- Containers/Docker/amd-scancode/scancodestatuscheck.py
+++ Containers/Docker/amd-scancode/scancodestatuscheck.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
+import click
 import scancodestatus
 import sys
 import os
-import logger
+import logging
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout)
+logger.setLevel(logging.DEBUG)
+
+click.command()
+@click.option('--dirpath', help='Directory path of scan result location')
 def errorchecking(dirpath):
     try:
-        path = dirpath 
+        path = dirpath
         # Check if path exits
         if os.path.exists(path):
             checkdirectory = scancodestatus.Scanstatus(path)
             exitcode = checkdirectory.scanLogResults()
             return exitcode
-        else:
-            return 1
     except OSError as err:
         logger.debug("OS error: {0}".format(err))
         logger.debug("Not able to find/list/sort the files")
@@ -24,6 +29,6 @@ def errorchecking(dirpath):
         return totalIssues
 
 if __name__ == "__main__":
-    statusexitcode = errorchecking(sys.argv[1])
+    statusexitcode = errorchecking()
     print(statusexitcode)
     sys.exit(statusexitcode)
