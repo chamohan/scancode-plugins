82fcc2dfb79d0e9959dfa43e1f59277e7e78dcc5
chamohan <63771787+chamohan@users.noreply.github.com>
2020-07-09 16:59:23 +0530

    re

--- plugins/scancode-scancodestatus/src/scancodestatus.py
+++ plugins/scancode-scancodestatus/src/scancodestatus.py
@@ -6,7 +6,7 @@ import datetime
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    format="{'time': %(asctime)-15s, 'filename': %(name)s, 'level':  %(levelname)s ,'linenumber': %(lineno)d, 'message': %(message)s"},
+    format="'time': %(asctime)-15s, 'filename': %(name)s, 'level':  %(levelname)s ,'linenumber': %(lineno)d, 'message': %(message)s",
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO,
     stream=sys.stdout)
